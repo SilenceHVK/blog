90987b0054ee24a3ecda16d42732578f2cefa559
hvkcoder <hvkcoder@gmail.com>
2020-07-22 21:37:43 +0800

    ➕ 添加 Eureka 使用

---
+++ java/spring-cloud-eureka/src/main/java/me/hvkcoder/spring_cloud_eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package me.hvkcoder.spring_cloud_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * TODO: Eureka Server
+ *
+ * @author h-vk
+ * @since 2020-02-14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
