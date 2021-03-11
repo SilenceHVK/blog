8e03b69716c7ada9ea805f58d1055be16d2409fd
hvkcoder <hvkcoder@gmail.com>
2021-03-11 16:26:19 +0800

    ➕ channel 实现生产者/消费模式

--- golang/basic/goroutine/channel_test.go
+++ golang/basic/goroutine/channel_test.go
@@ -7,8 +7,10 @@ package goroutine
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestChannel(t *testing.T) {
@@ -54,4 +56,107 @@ func TestDefineChannel(t *testing.T) {
 		channels[i] <- 'a' + i
 	}
 	wg.Wait()
+}
+
+// 使用 channel 实现生产者/消费者
+func TestProducerAndConsumer(t *testing.T) {
+	ch := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer close(ch)
+		for i := 0; i < 10; i++ {
+			num := rand.Int()
+			t.Log("生产者生产数据 -> ", num)
+			ch <- num
+			time.Sleep(time.Duration(500) * time.Microsecond)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for data := range ch {
+			t.Log("消费者消费数据 -> ", data)
+		}
+	}()
+	wg.Wait()
+	t.Log("程序结束......")
+}
+
+/*
+	计时器类型表示单个事件，当计时器过期时，当前时间将被发送到 Timer 结构体属性的 c 上，除非计时器是使用 After 创建的
+	type Timer struct {
+		C <-chan Time
+		r runtimeTimer
+	}
+*/
+func TestTimerChannel(t *testing.T) {
+	// 创建一个新的计时器，它会在至少持续时间 d 之后将当前时间发送到其 channel 上
+	newTimer := time.NewTimer(5 * time.Second)
+	t.Log("当前时间 = ", time.Now())
+	go func() {
+		date := <-newTimer.C
+		t.Log("newTimer 1s 后的时间 = ", date)
+	}()
+
+	// 停止定时器
+	newTimer.Stop()
+	// 重置计时器时间
+	newTimer.Reset(1 * time.Second)
+
+	// After 函数相当于 time.NewTimer(d).C
+	after := time.After(5 * time.Second)
+	t.Log("当前时间 = ", time.Now())
+	afterDate := <-after
+	t.Log("after 5s 后的时间 = ", afterDate)
+}
+
+/*
+Ticker 是一个定时触发的时间计时器，它会以一个间隔（interval）往 channel 发送一个时间（当前时间）
+而 channel 的接收者可以以固定的时间间隔从 channel 中读取事件
+*/
+func TestTickerChannel(t *testing.T) {
+	ticker := time.NewTicker(5 * time.Second)
+	num := 0
+	for {
+		num += 5
+		t.Log(<-ticker.C)
+
+		if num > 15 {
+			ticker.Stop()
+			t.Log("定时器结束")
+			break
+		}
+	}
+}
+
+/*
+ select 可以监听 channel 上的数据流动，语法与 switch 语法类似，如果没有 default，select 将被阻塞
+ select 有比较多的限制，其中最大的一条限制就是每个 case 语句里必须是一个 IO 操作
+*/
+func TestSelect(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	ch := make(chan int)
+	defer close(ch)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 8; i++ {
+			num := <-ch
+			t.Log(num)
+		}
+	}()
+
+	go func() {
+		a, b := 1, 1
+		for {
+			select {
+			case ch <- a:
+				a, b = b, a+b
+			}
+		}
+	}()
+	wg.Wait()
+	t.Log("程序结束")
 }
