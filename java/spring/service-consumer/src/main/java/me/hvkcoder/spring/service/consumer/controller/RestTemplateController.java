3c8823cace75af1909bd2a5828e7193f6fc0f14c
hvkcoder <hvkcoder@gmail.com>
2021-03-29 09:21:04 +0800

    ✨ 更改依赖

--- java/spring/service-consumer/src/main/java/me/hvkcoder/spring/service/consumer/controller/RestTemplateController.java
+++ java/spring/service-consumer/src/main/java/me/hvkcoder/spring/service/consumer/controller/RestTemplateController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -17,7 +15,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @since 2021/3/18
  */
 @RestController
-public class ConsumerController {
+@RequestMapping("/reset")
+public class RestTemplateController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
