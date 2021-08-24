96c94e26ebdc3a0f487b0266b377ec93f7a799e1
hvkcoder <hvkcoder@gmail.com>
2021-08-24 13:58:03 +0800

    ➕ hadoop3.X Dockerfile

---
+++ docker/hadoop/start-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+/etc/init.d/ssh start
+
+$HADOOP_HOME/bin/hdfs namenode -format
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+
+tail -f /dev/null
