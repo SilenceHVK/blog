7ebfa4952e0740636331b43db66b5fb7e5c0cdc4
H_VK <hvkcoder@gmail.com>
2022-02-12 14:55:05 +0800

    ➕ Spark WordCount

---
+++ java/bigdata/src/main/java/me/hvkcoder/park/WorkCountJava.java
@@ -0,0 +1,38 @@
+package me.hvkcoder.park;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.*;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Objects;
+
+/**
+ * Spark 实现单词统计
+ *
+ * @author h_vk
+ * @since 2022/2/11
+ */
+public class WorkCountJava {
+  public static void main(String[] args) {
+    SparkConf sparkConf = new SparkConf();
+    sparkConf.setMaster("local"); // 设置单机运行
+    sparkConf.setAppName("WordCountSparkJava");
+
+    JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+    JavaRDD<String> stringJavaRDD =
+        sparkContext.textFile(ClassLoader.getSystemResource("data.txt").getPath());
+    JavaPairRDD<String, Integer> result =
+        stringJavaRDD
+            .flatMap((FlatMapFunction<String, String>) s -> Arrays.stream(s.split(" ")).iterator())
+            .mapToPair((PairFunction<String, String, Integer>) s -> new Tuple2<>(s, 1))
+            .reduceByKey((Function2<Integer, Integer, Integer>) Integer::sum);
+    result.foreach(
+        (VoidFunction<Tuple2<String, Integer>>)
+            value -> System.out.println(value._1() + ":" + value._2()));
+  }
+}
---
+++ java/bigdata/src/main/scala/me/hvkcoder/park/WordCountScala.scala
@@ -0,0 +1,16 @@
+package me.hvkcoder.park
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCountScala {
+	def main(args: Array[String]): Unit = {
+		val sparkConf = new SparkConf()
+		sparkConf.setAppName("WorkCount")
+		sparkConf.setMaster("local") // 设置单机本地运行
+
+		val sparkContext = new SparkContext(sparkConf)
+		val textRdd = sparkContext.textFile(ClassLoader.getSystemResource("data.txt").getPath)
+		val result = textRdd.flatMap(_.split(" ")).map((_, 1)).reduceByKey((oldValue, value) => oldValue + value)
+		result.foreach(println)
+	}
+}
