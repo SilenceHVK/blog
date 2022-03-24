51f4a209fb25a7274ef5dd285a71ebedc75dae1c
H_VK <hvkcoder@gmail.com>
2022-03-24 15:16:52 +0800

    ⭐️ consul 反注册方法实现，并使用 signal 实现优雅关闭服务

--- golang/go-kit/main.go
+++ golang/go-kit/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/SilenceHVK/blog/golang/go-kit/endpoint"
 	"github.com/SilenceHVK/blog/golang/go-kit/service"
 	"github.com/SilenceHVK/blog/golang/go-kit/transport"
 	"github.com/SilenceHVK/blog/golang/go-kit/utils"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -17,6 +22,24 @@ func main() {
 	r := mux.NewRouter()
 	r.Methods(http.MethodGet).Path("/user/{uid:[0-9]+}").Handler(httptransport.NewServer(serviceEndpoint, transport.DecodeUserServiceRequest, transport.EncodeUserServiceResponse))
 
-	utils.RegisterService()
-	http.ListenAndServe(":9999", r)
+	errChan := make(chan error)
+	go func() {
+		utils.RegisterService()
+		err := http.ListenAndServe(":9999", r)
+		if err != nil {
+			log.Println(err)
+			errChan <- err
+		}
+	}()
+
+	go func() {
+		// 监听服务信号
+		sigC := make(chan os.Signal)
+		signal.Notify(sigC, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-sigC)
+	}()
+
+	getError := <-errChan
+	utils.UnRegister()
+	log.Println(getError)
 }
--- golang/go-kit/utils/consul.go
+++ golang/go-kit/utils/consul.go
@@ -2,6 +2,20 @@ package utils
 
 import consul "github.com/hashicorp/consul/api"
 
+var consulClient *consul.Client
+
+func init() {
+	// 指定 Consul 注册中心地址
+	config := consul.DefaultConfig()
+	config.Address = "192.168.50.130:55001"
+
+	client, err := consul.NewClient(config)
+	if err != nil {
+		panic(err)
+	}
+	consulClient = client
+}
+
 func RegisterService() {
 	//  指定服务注册信息
 	registration := &consul.AgentServiceRegistration{
@@ -11,13 +25,9 @@ func RegisterService() {
 		Port:    9999,
 	}
 
-	// 指定 Consul 注册中心地址
-	config := consul.DefaultConfig()
-	config.Address = "192.168.50.130:55001"
+	_ = consulClient.Agent().ServiceRegister(registration)
+}
 
-	client, err := consul.NewClient(config)
-	if err != nil {
-		panic(err)
-	}
-	_ = client.Agent().ServiceRegister(registration)
+func UnRegister() {
+	_ = consulClient.Agent().ServiceDeregister("USER-SERVICE")
 }
