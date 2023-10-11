a3f7a959f31db0c5bd4ffacc818312f941db5efc
H_VK <hvkcoder@gmail.com>
2023-10-11 14:11:05 +0800

    ➕实现冒泡排序

--- java/java-basic/src/main/java/me/hvkcoder/java_basic/algorithm/SortBubble.java
+++ java/java-basic/src/main/java/me/hvkcoder/java_basic/algorithm/SortBubble.java
@@ -7,61 +7,53 @@ import me.hvkcoder.java_basic.algorithm.visualAlgo.AlgoVisHelper;
 import javax.swing.*;
 import java.awt.*;
 
-
 /**
- * 选择排序
+ * 冒泡排序
  *
  * @author h_vk
- * @since 2023/10/10
+ * @since 2023/10/11
  */
-public class SelectionSort {
+public class SortBubble {
+
 	public static void main(String[] args) {
 		int DELAY = 40;
 		SortData sortData = new SortData(50, 400);
 		int[] data = sortData.getNumbers();
 
 		EventQueue.invokeLater(() -> {
-			AlgoFrame frame = new AlgoFrame("选择排序", (g2d, dimension) -> {
+			AlgoFrame frame = new AlgoFrame("冒泡排序", (g2d, dimension) -> {
 				int canvasWidth = (int) dimension.getWidth();
 				int canvasHeight = (int) dimension.getHeight();
 
 				int w = canvasWidth / sortData.getSize();
 				for (int i = 0; i < sortData.getSize(); i++) {
-					if (i < sortData.getOrderedIndex()) {
+					if (i == sortData.getCurrentCompareIndex() || i == sortData.getCurrentCompareNextIndex()) {
 						AlgoVisHelper.setColor(g2d, AlgoVisHelper.Green);
 					} else {
-						AlgoVisHelper.setColor(g2d, AlgoVisHelper.Grey);
-					}
-
-					if (i == sortData.getCurrentMinIndex()) {
 						AlgoVisHelper.setColor(g2d, AlgoVisHelper.Blue);
 					}
-
-					if (i == sortData.getCurrentCompareIndex()) {
-						AlgoVisHelper.setColor(g2d, AlgoVisHelper.Red);
+					if (i >= sortData.getOrderedIndex()) {
+						AlgoVisHelper.setColor(g2d, AlgoVisHelper.Orange);
 					}
 					AlgoVisHelper.fillRectangle(g2d, i * w + 1, canvasHeight - data[i], w - 1, data[i]);
 				}
 			});
 
 			frame.animation(DELAY, () -> {
-				for (int i = 0; i < sortData.getSize(); i++) {
-					int min = i;
-					sortData.setDataIndex(i, min, -1, frame, DELAY);
-					for (int j = i + 1; j < sortData.getSize(); j++) {
-						sortData.setDataIndex(i, min, j, frame, DELAY);
-						if (data[min] > data[j]) {
-							min = j;
-							sortData.setDataIndex(i, min, min, frame, DELAY);
+				int size = sortData.getSize();
+				for (int i = 0; i < size; i++) {
+					int sortedIndex = size - 1 - i;
+					for (int j = 0; j < sortedIndex; j++) {
+						sortData.setDataIndex(sortData.getOrderedIndex(), j, j + 1, frame, DELAY);
+						if (data[j] > data[j + 1]) {
+							int tmp = data[j];
+							data[j] = data[j + 1];
+							data[j + 1] = tmp;
 						}
 					}
-					if (min != i) {
-						int tmp = data[min];
-						data[min] = data[i];
-						data[i] = tmp;
-					}
+					sortData.setDataIndex(sortedIndex, -1, -1, frame, DELAY);
 				}
-				sortData.setDataIndex(sortData.getSize(), -1, -1, frame, DELAY);
+				sortData.setDataIndex(Integer.MAX_VALUE, -1, -1, frame, DELAY);
 				Thread.currentThread().interrupt();
 				System.out.println("排序完成");
 			});
@@ -73,15 +65,15 @@ public class SelectionSort {
 		private int[] numbers;
 		private final int size;
 		private int orderedIndex; // 已排好序的索引区间
-		private int currentMinIndex; // 当前最小元素的索引
 		private int currentCompareIndex; // 当前比较元素的索引
+		private int currentCompareNextIndex; // 当前比较元素的下一个索引
 
 		public SortData(int size, int maxValue) {
 			this.size = size;
 			this.generatorData(size, maxValue);
-			this.orderedIndex = -1;
-			this.currentMinIndex = -1;
+			this.orderedIndex = Integer.MAX_VALUE;
 			this.currentCompareIndex = -1;
+			this.currentCompareNextIndex = -1;
 		}
 
 		private void generatorData(int size, int maxValue) {
@@ -91,11 +83,11 @@ public class SelectionSort {
 			}
 		}
 
-		public void setDataIndex(int orderedIndex, int currentMinIndex, int currentCompareIndex, JFrame frame, int delay) {
+		public void setDataIndex(int orderedIndex, int currentCompareIndex, int currentCompareNextIndex, JFrame frame, int delay) {
 			try {
 				this.orderedIndex = orderedIndex;
-				this.currentMinIndex = currentMinIndex;
 				this.currentCompareIndex = currentCompareIndex;
+				this.currentCompareNextIndex = currentCompareNextIndex;
 				frame.repaint();
 				Thread.sleep(delay);
 			} catch (InterruptedException e) {
