0d18e64bcf12739221a72e620da128509b27f6e4
H_VK <hvkcoder@gmail.com>
2023-10-15 10:47:16 +0800

    ➕ Scala 实现选择排序

--- java/scala-basic/src/main/scala/me/hvkcoder/algorithm/SortSelection.scala
+++ java/scala-basic/src/main/scala/me/hvkcoder/algorithm/SortSelection.scala
@@ -1,5 +1,34 @@
 package me.hvkcoder.algorithm
 
-object SortSelection {
+import scala.util.Random
 
+// 选择排序
+object SortSelection {
+	def main(args: Array[String]): Unit = {
+		val size = 10
+		val numbers = new Array[Int](size)
+		for (i <- 0 until size) {
+			numbers(i) = Random.nextInt(100)
+		}
+		print("未排序的数据 => ")
+		numbers.foreach(item => print(f"$item "))
+
+		for (i <- numbers.indices) {
+			var min = i
+			for (j <- i + 1 until numbers.length) {
+				if (numbers(min) > numbers(j)) {
+					min = j
+				}
+			}
+
+			if (min != i) {
+				val tmp = numbers(i)
+				numbers(i) = numbers(min)
+				numbers(min) = tmp
+			}
+		}
+		println()
+		print("选择排序结果 => ")
+		numbers.foreach(item => print(f"$item "))
+	}
 }
